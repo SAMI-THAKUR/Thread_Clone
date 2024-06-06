48cd62c51b7ab1f3d920d7f5d9d6095aec3e5e03
SAMI THAKUR <2022.sami.thakur@ves.ac.in>
2024-06-06 11:10:23 +0530

    render

--- server/app.js
+++ server/app.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(
   cors({
-    origin: "https://thread-clone-bjsq.vercel.app/",
     credentials: true,
+    origin: ["https://thread-clone-bjsq.vercel.app/", "https://thread-clone-pi-gules.vercel.app/"],
   }),
 );
 cloudinary.config({
