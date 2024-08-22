27e231a89d22ca762fb90674b53322c1e6f8cce2
SAMI THAKUR <2022.sami.thakur@ves.ac.in>
2024-08-23 00:09:43 +0530

    axios

--- server/utils/helpers/generateJWT.js
+++ server/utils/helpers/generateJWT.js
@@ -10,6 +10,7 @@ const createToken = (id, res) => {
     httpOnly: true, // cookie cannot be accessed by client side javascript
     secure: true, // Set to true in production
     maxAge: 3 * 24 * 60 * 60 * 1000, // cookie is sent only to the same site as the one that originated it
+    sameSite: "None", // Required for cross-site requests
   });
 };
 
